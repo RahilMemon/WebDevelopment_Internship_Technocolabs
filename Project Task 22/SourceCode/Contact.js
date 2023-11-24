958b6146c6c7b427e3c2e5d4dea63e9c64c667ae
RahilMemon <memonrahil2424@gmail.com>
2023-11-24 18:35:52 +0530

    Task done 21 22 23

---
+++ Project Task 22/SourceCode/Contact.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    let headingMessage;
+    const login = (
+      <form
+        action="#"
+        onSubmit={this.authorize}>
+        <input
+          type="password"
+          placeholder="Password" />
+        <input
+          type="submit" />
+      </form>
+    );
+    const contactInfo = (
+      <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+    );
+    if(this.state.authorized) {
+      headingMessage = "Contact";
+    } else {
+      headingMessage = "Enter the Password";
+    }
+    return (
+      <div id="authorization">
+        <h1>{headingMessage}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Contact />, 
+  document.getElementById('app')
+);
